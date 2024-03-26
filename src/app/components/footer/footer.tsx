687e3384fd7414e32d767b830438ccc9c284df50
vignesh2403 <vigneshperiyasamy1999@gmail.com>
2024-03-26 15:28:34 +0530

    initial header and footer

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import './footer.scss'
+
+export default function Footer(){
+    return(
+        <div className="footer-container">
+            <div className='email'>Gmail: vignesh.sof.eng@gmail.com</div>
+        </div>
+    )
+}
---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,13 @@
+import './header.scss'
+
+export default function Header(){
+    return(
+        <div className="header-container">
+            <div className='name-container'>
+            <span className='name'>VIGNESH P</span>
+                <br></br>
+                <span className='designation'>Software Engineer</span>
+            </div>
+        </div>
+    )
+}
