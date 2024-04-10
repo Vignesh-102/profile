723f057bb11b4e559b8709a021d16825ddfd4cb6
Vignesh-102 <vignesh.sof.eng@gmail.com>
2024-04-10 19:20:38 +0530

    updated footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.module.css";
 import Header from "./components/header/header"
 import Footer from "./components/footer/footer";
 
 export default function Home() {
   return (
     <div className="page-container">
-      <Header></Header>
-      <Footer></Footer>
+      <div className="header">
+        <Header></Header>
+      </div>
+      <div className="footer">
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
