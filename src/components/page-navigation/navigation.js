cda1d4eb2cb33234b5d7c4f14917df90b7a64f55
vignesh-102 <vigneshperiyasamy1999@gmail.com>
2025-12-17 17:54:19 +0530

    link fix

--- src/components/page-navigation/navigation.js
+++ src/components/page-navigation/navigation.js
@@ -4,11 +4,11 @@ import styles from './navigation.module.css';
 export default function Navigation() {
   return (
     <nav className={styles.navbar}>
-      <NavLink to="/" className={({ isActive }) => isActive ? styles.active : ''}>Home</NavLink>
-      <NavLink to="/experience" className={({ isActive }) => isActive ? styles.active : ''}>Experience</NavLink>
-      <NavLink to="/skills" className={({ isActive }) => isActive ? styles.active : ''}>Skills</NavLink>
-      <NavLink to="/projects" className={({ isActive }) => isActive ? styles.active : ''}>Personal Projects</NavLink>
-      <NavLink to="/education" className={({ isActive }) => isActive ? styles.active : ''}>Education</NavLink>
+      <NavLink to="" className={({ isActive }) => isActive ? styles.active : ''}>Home</NavLink>
+      <NavLink to="experience" className={({ isActive }) => isActive ? styles.active : ''}>Experience</NavLink>
+      <NavLink to="skills" className={({ isActive }) => isActive ? styles.active : ''}>Skills</NavLink>
+      <NavLink to="projects" className={({ isActive }) => isActive ? styles.active : ''}>Personal Projects</NavLink>
+      <NavLink to="education" className={({ isActive }) => isActive ? styles.active : ''}>Education</NavLink>
     </nav>
   );
 }
