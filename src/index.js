5f6ffdd44ca71ff6b04628442e67b7dffcd3aef0
vignesh-102 <vigneshperiyasamy1999@gmail.com>
2025-12-17 17:33:53 +0530

    has router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { hashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/profile'>
+    <hashRouter>
       <App />
-    </BrowserRouter>
+    </hashRouter>
   </React.StrictMode>
 );
 reportWebVitals();
